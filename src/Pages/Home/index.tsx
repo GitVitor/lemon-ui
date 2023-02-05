67ea38c650b63e5643f53db8b434e2473e4a55fa
Vitor Costa Jose <vitorcostajose@MacBook-Pro-de-Vitor.local>
2023-02-05 14:43:22 -0300

    adjust style of Home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,13 +1,27 @@
 import { Header } from "../../Components/Header";
 import * as s from "./styles";
 
+const StarSVG = () => {
+  return (
+    <>
+      <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M8 1.007A7 7 0 011 8a7 7 0 017 7 7 7 0 017-7 7 7 0 01-7-7v.007z"
+          fill="currentColor"
+        ></path>
+      </svg>
+    </>
+  );
+};
+
 export const HomePage = () => {
   return (
     <>
       <Header />
       <s.Container>
         <header className="App-header">
-          chegou a hora de escolher a sua energia
+          <StarSVG />
+          <h1>chegou a hora de escolher a sua energia</h1>
         </header>
         <p>
           Com a Lemon, você sempre – e é sempre mesmo – vai ter uma energia mais
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -2,6 +2,37 @@ import styled from "styled-components";
 import { Container as OriginalContainer } from "../../Components/Container";
 
 export const Container = styled(OriginalContainer)`
+  ${({ theme }) => `
   background-color: #008059;
   height: 100%;
+
+  padding: ${theme.space(2)}px;
+
+  header,
+  p {
+    color: white;
+    font-weight: bold;
+    padding: ${theme.space(4)}px;
+  }
+
+  p {
+    line-height: 1.5rem;
+  }
+
+  h1 {
+    font-size: 2rem;
+    line-height: 2.5rem;
+  }
+
+  header {
+    margin: ${theme.space(16)}px 0 ${theme.space(4)}px;
+
+    svg {
+      height: 24px;
+      color: #8affb6;
+      margin-bottom: ${theme.space(4)}px;
+
+    }
+  }
+  `}
 `;
